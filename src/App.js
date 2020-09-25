2d683c2194d66098c2ede2fcf2822439c7ddd5e0
dubianets <vadikdubianets@gmail.com>
2020-09-25 00:03:46 -0700

    add button

--- src/App.js
+++ src/App.js
@@ -1,44 +1,87 @@
 import React, {useState} from 'react';
+import List from "./List";
+import Controller from "./Controller";
 
 
 const initialCounters = [
-    {id: 1, title: 'Green', count: 5},
-    {id: 2, title: 'Pink', count: 50},
-    {id: 3, title: 'Orange', count: 20}
+    {id: 1, title: 'Green', count: 5, editInput: false},
+    {id: 2, title: 'Pink', count: 50, editInput: false},
+    {id: 3, title: 'Orange', count: 20, editInput: false}
 ]
 
 function App() {
 
-    const [counter, setCounters] = useState(initialCounters);
+    // const deleteById = (id) =>  {
+    //    console.log(id);
+    //    const filterOfCounter = list.filter(el => el.id !== id );
+    //     setList(filterOfCounter);
+    // }
+    const [list, setList] = useState(initialCounters);
 
-    const deleteById = (id) =>  {
-       console.log(id);
-       const filterOfCounter = counter.filter(el => el.id !== id );
-        setCounters(filterOfCounter);
+    const inputButtonSet = (index) => {
+        const newInputButton = list.map((el, i) => {
+            if(index === i ) return {...el, editInput: !el.editInput}
+            return el;
+        })
+
+        setList(newInputButton);
+    }
+
+
+
+    const swapCounter = (index, val) => {
+        const newList = [...list];
+        const currentEl = newList[index];
+        const preveesEl = newList[index + val];
+        newList[index] = preveesEl;
+        newList[index + val] = currentEl;
+        setList(newList);
+    }
+
+
+
+    const inputCounter = (value, index) => {
+        const newInputCounter = list.map((el, i) => {
+            if(index === i) return {...el, count: value, editInput: !el.editInput};
+            return el;
+        })
+        setList(newInputCounter);
+
     }
 
     return (
+
         <div>
-            counters
 
-            <ul>
+            <div>
+                <Controller/>
+            </div>
 
-                {counter.map(el => <li key={el.id}>  {el.title}
-                    <button>-</button>
-                    {el.count}
-                    <button>+</button>
-                    <button>+2</button>
-                    <button onClick={ () => deleteById(el.id)}>delete</button>
-                </li>)}
-
-            </ul>
-
-            <input type="text" placeholder= "Counter title"/>
-            <input type="text" placeholder = "Counter value"/>
-            <button>Add counter</button>
+       <List list={list} key={Math.random()} swapCounter={swapCounter} inputButtonSet={inputButtonSet} inputCounter={inputCounter} />
 
         </div>
+
     );
+        // <div>
+        //     counters
+        //
+        //     <ul>
+        //
+        //         {list.map(el => <li key={el.id}>  {el.title}
+        //             <button>-</button>
+        //             {el.count}
+        //             <button>+</button>
+        //             <button>+2</button>
+        //             <button onClick={ () => deleteById(el.id)}>delete</button>
+        //         </li>)}
+        //
+        //     </ul>
+        //
+        //     <input type="text" placeholder= "Counter title"/>
+        //     <input type="text" placeholder = "Counter value"/>
+        //     <button>Add counter</button>
+        //
+        // </div>
 }
 
 export default App;
---
+++ src/Controller.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+function Controller() {
+
+    return (
+        <div>
+
+            <input type='text' placeholder='Counter Title'/>
+            <input type='text' placeholder='Counter value'/>
+            <button>add counter</button>
+            <hr/>
+
+        </div>
+    )
+}
+
+export default Controller;
